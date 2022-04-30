2f40e3d5bd652e847d8a26d1a428590b0686104b
Fireclaw <fireclawthefox@gmail.com>
2022-04-30 16:34:29 +0200

    Version up to 22.04.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SceneEditor",
-    version="22.04",
+    version="22.04.1",
     author="Fireclaw",
     author_email="fireclawthefox@gmail.com",
     description="A scene editor for the Panda3D engine",
