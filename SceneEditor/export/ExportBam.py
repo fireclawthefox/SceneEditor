f3956bd5fd0ebd50069ae0ba7408bf1e1d144e2e
Fireclaw <fireclawthefox@gmail.com>
2022-12-28 10:00:44 +0100

    Use DirectFolderBrowser ask for overwrite
    Removed custom ask for overwrite dialogs
    Enable DirectFolderBrowser ask for overwrite dialog
    Set titles for browser

--- SceneEditor/export/ExportBam.py
+++ SceneEditor/export/ExportBam.py
@@ -37,7 +37,9 @@ class ExporterBam:
             save_path,
             save_file,
             [".bam"],
-            tooltip)
+            tooltip,
+            askForOverwrite=True,
+            title="Export Scene as BAM")
         self.browser.show()
 
     def cleanup_np(self, root_np):
@@ -83,40 +85,13 @@ class ExporterBam:
 
     def save(self, doSave):
         if doSave:
-            self.dlgOverwrite = None
-            self.dlgOverwriteShadow = None
             path = self.browser.get()
             path = os.path.expanduser(path)
             path = os.path.expandvars(path)
-            if os.path.exists(path):
-                self.dlgOverwrite = YesNoDialog(
-                    text="File already Exist.\nOverwrite?",
-                    relief=DGG.RIDGE,
-                    frameColor=(1,1,1,1),
-                    frameSize=(-0.5,0.5,-0.3,0.2),
-                    sortOrder=1,
-                    button_relief=DGG.FLAT,
-                    button_frameColor=(0.8, 0.8, 0.8, 1),
-                    command=self.__executeSave,
-                    extraArgs=[path],
-                    scale=300,
-                    pos=(base.getSize()[0]/2, 0, -base.getSize()[1]/2),
-                    parent=base.pixel2d)
-                self.dlgOverwriteShadow = DirectFrame(
-                    pos=(base.getSize()[0]/2 + 10, 0, -base.getSize()[1]/2 - 10),
-                    sortOrder=0,
-                    frameColor=(0,0,0,0.5),
-                    frameSize=self.dlgOverwrite.bounds,
-                    scale=300,
-                    parent=base.pixel2d)
-            else:
-                self.__executeSave(True, path)
+            self.__executeSave(path)
             base.messenger.send("setLastPath", [path])
         self.browser.destroy()
         del self.browser
 
-    def __executeSave(self, overwrite, path):
-        if self.dlgOverwrite is not None: self.dlgOverwrite.destroy()
-        if self.dlgOverwriteShadow is not None: self.dlgOverwriteShadow.destroy()
-        if not overwrite: return
+    def __executeSave(self, path):
         self.export_scene_np.writeBamFile(path)
--- SceneEditor/export/ExportProject.py
+++ SceneEditor/export/ExportProject.py
@@ -40,64 +40,33 @@ class ExporterProject:
             True,
             save_path,
             save_file,
-            tooltip=tooltip)
+            tooltip=tooltip,
+            askForOverwrite=True,
+            title="Save Scene")
         self.browser.show()
 
     def excSave(self):
-        self.dlgOverwrite = None
-        self.dlgOverwriteShadow = None
-
         tmpPath = os.path.join(tempfile.gettempdir(), "SEExceptionSave.scene")
-        self.__executeSave(True, tmpPath)
+        self.__executeSave(tmpPath)
         logging.info("Wrote crash session file to {}".format(tmpPath))
 
     def autoSave(self, fileName=""):
-        self.dlgOverwrite = None
-        self.dlgOverwriteShadow = None
         if fileName == "":
             fileName = os.path.join(tempfile.gettempdir(), "SEAutosave.scene")
-        self.__executeSave(True, fileName)
+        self.__executeSave(fileName)
         logging.info("Wrote autosave file to {}".format(fileName))
 
     def save(self, doSave):
         if doSave:
-            self.dlgOverwrite = None
-            self.dlgOverwriteShadow = None
             path = self.browser.get()
             path = os.path.expanduser(path)
             path = os.path.expandvars(path)
-            if os.path.exists(path):
-                self.dlgOverwrite = YesNoDialog(
-                    text="File already Exist.\nOverwrite?",
-                    relief=DGG.RIDGE,
-                    frameColor=(1,1,1,1),
-                    frameSize=(-0.5,0.5,-0.3,0.2),
-                    sortOrder=1,
-                    button_relief=DGG.FLAT,
-                    button_frameColor=(0.8, 0.8, 0.8, 1),
-                    command=self.__executeSave,
-                    extraArgs=[path],
-                    scale=300,
-                    pos=(base.getSize()[0]/2, 0, -base.getSize()[1]/2),
-                    parent=base.pixel2d)
-                self.dlgOverwriteShadow = DirectFrame(
-                    pos=(base.getSize()[0]/2 + 10, 0, -base.getSize()[1]/2 - 10),
-                    sortOrder=0,
-                    frameColor=(0,0,0,0.5),
-                    frameSize=self.dlgOverwrite.bounds,
-                    scale=300,
-                    parent=base.pixel2d)
-            else:
-                self.__executeSave(True, path)
+            self.__executeSave(path)
             base.messenger.send("setLastPath", [path])
         self.browser.destroy()
         del self.browser
 
-    def __executeSave(self, overwrite, path):
-        if self.dlgOverwrite is not None: self.dlgOverwrite.destroy()
-        if self.dlgOverwriteShadow is not None: self.dlgOverwriteShadow.destroy()
-        if not overwrite: return
-
+    def __executeSave(self, path):
         jsonTools = JSONTools()
         jsonElements = jsonTools.getProjectJSON(self.objects, self.scene_root)
         with open(path, 'w') as outfile:
